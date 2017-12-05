5d423357b63327b1c96c856494a849c7988d4064
Nova <robloxmuffinzcafe@gmail.com>
2017-12-05 15:33:36 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -52,3 +52,22 @@ function embed(title,description,name,value,inline) {
     send(resp);
   }
 }
+
+function rNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function userRoles(id) {
+  //var letters = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s"]
+  if(!id) return send("Error! You must have a UserID for \`userRoles\`! \`\`\`js\nuserRoles(id)\`\`\`");
+  if(id.match(/[a-z]/i == true) return send("Nope! The UserID does not contain any letters and/or special characters");
+  var roles = new Array();
+  for (var x = 0; x < ServerRoles..length; x++) {
+    if(MemberHasRole(UserID, ServerRoles[x])) {
+      roles.push(ServerRoles[x]);
+    } else {
+      //Do nothing
+    }
+  }
+  send(roles.slice(1).join("\n"));
+}
