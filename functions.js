19e5a8d733281ea587cbc6e432658ab3321ab343
Nova <robloxmuffinzcafe@gmail.com>
2017-12-05 15:35:08 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -8,41 +8,28 @@ function embed(title,description,name,value,inline) {
   var f1 = {};
   if(!title) {
     send("You must at least supply a Title!");
-    
   } else {
-    
     resp.title = title;
     if(!description) {
       //Do nothing
-      
     } else {
-      
       resp.description = description;
     }
     if(!name) {
       //Do nothing
-      
     } else {
-      
       f1.name = name;
       if(!value) {
         f1.value = "⠀";
-        
       } else {
-        
         f1.value = value;
       }
-      
       if(!inline) {
         f1.inline = false;
-        
       } else {
-        
         if(inline != "true" && inline != "false") {
           send("You can only choose the options `true` and `false` for the **inline**!");
-          
         } else {
-          
           f1.inline = inline;
         }
       }
