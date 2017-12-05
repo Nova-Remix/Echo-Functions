b99d1161a588d3a9b2bb15f26cd4bbedb2cacd7c
Nova <robloxmuffinzcafe@gmail.com>
2017-12-05 09:56:53 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,10 +2,16 @@ function send(text) {
   return resp = text;
 }
 
-function embed(title,description) {
+function embed(title,description,name,value,inline) {
   var resp = {};
   var fields = new Array();
-  resp.title = title;
-  resp.description = description;
+  var f1 = {};
+  resp.title = title || "";
+  resp.description = description || "";
+  f1.name = name || "";
+  f1.value = value || "";
+  f1.inline = inline || false;
+  fields.push(f1);
+  resp.fields = fields;
   send(resp);
 }
