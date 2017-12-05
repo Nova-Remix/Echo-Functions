3fcd106da09c552dc25a86ce76b2ae93d217991b
Nova <robloxmuffinzcafe@gmail.com>
2017-12-05 10:50:56 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,10 +6,10 @@ function embed(title,description,name,value,inline) {
   var resp = {};
   var fields = new Array();
   var f1 = {};
-  resp.title = title || "";
-  resp.description = description || "";
-  f1.name = name || "";
-  f1.value = value || "";
+  resp.title = title || "⠀";
+  resp.description = description || "⠀";
+  f1.name = name || "⠀";
+  f1.value = value || "⠀";
   f1.inline = inline || false;
   fields.push(f1);
   resp.fields = fields;
