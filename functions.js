9d3745afcba85b87826b7fd18d3ffb6b78476d0a
Nova <robloxmuffinzcafe@gmail.com>
2017-12-06 15:21:10 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -45,9 +45,14 @@ function rNum(min, max) {
 }
 
 function userRoles(id) {
-  //var letters = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s"]
+  var letters = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"]
   if(!id) return send("Error! You must have a UserID for \`userRoles\`! \`\`\`js\nuserRoles(id)\`\`\`");
-  if(id.match(/[a-z]/i) == true) return send("Nope! The UserID does not contain any letters and/or special characters");
+  
+  for(var i = 0; i < letters.length; i++) {
+    if(id.hasOwnProperty(letters[i])) {
+      send("Nope! The UserID does not contain any letters and/or special characters");
+    }
+  }
   var myroles = new Array();
   for (var x = 0; x < Server.Roles.length; x++) {
     if(MemberHasRole(id, Server.Roles[x].Name)) {
