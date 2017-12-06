86b07e31a7b53c15c6f0dd74a258920d00596a1a
Nova <robloxmuffinzcafe@gmail.com>
2017-12-06 13:51:06 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -48,13 +48,13 @@ function userRoles(id) {
   //var letters = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g","H","h","I","i","J","j","K","k","L","l","M","m","N","n","O","o","P","p","Q","q","R","r","S","s"]
   if(!id) return send("Error! You must have a UserID for \`userRoles\`! \`\`\`js\nuserRoles(id)\`\`\`");
   //if(id.match(/[a-z]/i) == true) return send("Nope! The UserID does not contain any letters and/or special characters");
-  var roles = new Array();
-  for (var x = 0; x < ServerRoles.length; x++) {
-    if(MemberHasRole(id, ServerRoles[x])) {
-      roles.push(ServerRoles[x]);
+  var myroles = new Array();
+  for (var x = 0; x < Server.Roles.length; x++) {
+    if(MemberHasRole(id, Server.Roles[x])) {
+      myroles.push(Server.Roles[x].Name);
     } else {
       //Do nothing
     }
   }
-  return send(JSON.stringify(ServerRoles) + roles.join("\n"));
+  return send(roles.join("\n"));
 }
