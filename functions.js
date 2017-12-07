d00d65e548c6034dc9d0f3f3f60344dba60acbfe
Nova <robloxmuffinzcafe@gmail.com>
2017-12-07 11:08:57 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 function send(text) {
-  if(!text) return resp = "I can't send an empty message! Please examine the **send()** function carefully.
+  if(!text) return resp = "I can't send an empty message! Please examine the **send()** function carefully.";
   return resp = text;
 }
 
