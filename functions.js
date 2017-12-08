ec05a20fecbc3f023983638bc78acec00ad298c7
Nova <robloxmuffinzcafe@gmail.com>
2017-12-08 08:09:06 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,8 @@
 //SEND
 function send(text) {
   if(!text) return resp = "I can't send an empty message! Please examine the **send()** function carefully.";
+  text = text.replace('${{', '" + ');
+  text = text.replace('}}', ' + "');
   return resp = text;
 }
 
