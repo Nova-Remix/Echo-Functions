68f08307fe5556cb93b70842fa4676578b2f8ce2
Nova <robloxmuffinzcafe@gmail.com>
2017-12-19 10:22:29 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -626,18 +626,20 @@ function getAPI(api, grab) {
     return send("You must provide information for me to grab from the API!");
   } else if(api.indexOf("http") === -1 && api.indexOf("www.") === -1) {
     return send("The link must includes `https://` or `http://` as well as include `www.`!");
-  } else if(grab.indexOf("[") === -1 || grab.indexOf("]") === -1) {
+  } else if(grab.indexOf('["') === -1 || grab.indexOf('"]') === -1) {
     return send("You must follow this format to grab information from the API:\n**[\"Movies\"][\"Genres\"][\"Horror\"]**");
   } else {
     try {
       var obj = GetJSON(api);
       var info = grab.replace(/["/g, "");
       info = info.replace(" ", "");
-      info = info.split("\"]");
+      info = info.split('"]');
      for (var e = 0; e < info.length; e++) {
        if(!obj.hasOwnProperty(info[e])) {
          return send("Uh oh! I ran into a problem trying to find the value **" + info[e] + "** in the JSON API! Please check your spelling and make sure this value exists!");
       }
+    } catch (err) {
+      send("Error found!\n```" + err + "```");
     }
   }
 }
