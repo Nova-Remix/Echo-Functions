a6b5ae55ff816445b7557e7c7c35cb66b5c231f4
Nova <robloxmuffinzcafe@gmail.com>
2017-12-19 10:11:10 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -274,76 +274,7 @@ function makeBitwise(perm1) {
 /*========================================================================
 UN BITWISE
 =========================================*/
-function unBitwise(perm1, perm2, perm3) {
-      var perms = {
-      "0x00000001":"CREATE_INSTANT_INVITE",
-      "0x00000002":"KICK_MEMBERS",
-      "0x00000004":"BAN_MEMBERS",
-      "0x00000008":"ADMINISTRATOR",
-      "0x00000010":"MANAGE_CHANNELS",
-      "0x00000020":"MANAGE_GUILD",
-      "0x00000040":"ADD_REACTIONS",
-      "0x00000080":"VIEW_AUDIT_LOG",
-      "0x00000400":"VIEW_CHANNEL",
-      "0x00000800":"SEND_MESSAGES",
-      "0x00001000":"SEND_TTS_MESSAGES",
-      "0x00002000":"MANAGE_MESSAGES",
-      "0x00004000":"EMBED_LINKS",
-      "0x00008000":"ATTACH_FILES",
-      "0x00010000":"READ_MESSAGE_HISTORY",
-      "0x00020000":"MENTION_EVERYONE",
-      "0x00040000":"USE_EXTERNAL_EMOJIS",
-      "0x00100000":"CONNECT",
-      "0x00200000":"SPEAK",
-      "0x00400000":"MUTE_MEMBERS",
-      "0x00800000":"DEAFEN_MEMBERS",
-      "0x01000000":"MOVE_MEMBERS",
-      "0x02000000":"USE_VAD",
-      "0x04000000":"CHANGE_NICKNAME",
-      "0x08000000":"MANAGE_NICKNAMES",
-      "0x10000000":"MANAGE_ROLES",
-      "0x20000000":"MANAGE_WEBHOOKS",
-      "0x40000000":"MANAGE_EMOJIS"
-    }
-      
-  if(!perm1) {
-    send("You need to add in the permission name to grab the bitwise number of!");
-  } else {
-    if(perms[perm1]) {
-      var x = perms[perm1];
-    } else {
-      return send("Sorry, but **" + perm1 + "** isn't valid yet.");
-    }
-  }
-  
-  if(!perm2) {
-    return x;
-  } else {
-    if(perms[perm2]) {
-      x = x + ", " + perms[perm2];
-    } else {
-      return send("Sorry, but **" + perm2 + "** isn't valid yet.");
-    }
-  }
-  
-  if(!perm3) {
-    return x;
-  } else {
-    if(perms[perm3]) {
-      x = x + ", " + perms[perm3];
-      return x;
-    } else {
-      return send("Sorry, but **" + perm3 + "** isn't valid yet.");
-    }
-  }
-}
-
-
-
-
-
-
-/*function unBitwise(count, type) {
+function unBitwise(count, type) {
     var type = (typeof type !== 'undefined') ?  type : 0;
     var count = parseInt(count);
     if (count.toString() !== 'NaN') {
@@ -649,7 +580,7 @@ function unBitwise(perm1, perm2, perm3) {
     } else {
         return "Error : " + count + "is not a valid number";
     }
-}*/
+}
 /*========================
 SERVER CHANNELS (ID, NAME)
 =======================*/
@@ -701,7 +632,7 @@ function getAPI(api, grab) {
     try {
       var obj = GetJSON(api);
       var info = grab.replace(/["/g, "");
-      info = info.replace(/ /g, "");
+      info = info.replace(" ", "");
       info = info.split("\"]");
      for (var e = 0; e < info.length; e++) {
        if(!obj.hasOwnProperty(info[e])) {
