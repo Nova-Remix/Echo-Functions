99af39734543d6400545dfb99a9e049bf26aa9ac
Nova <robloxmuffinzcafe@gmail.com>
2018-01-09 17:31:08 -0500

    Update ghostfunctions.js

--- ghostfunctions.js
+++ ghostfunctions.js
@@ -317,7 +317,7 @@ function newGhost() {
 
   Player[RawUserID + "-Ghost"] = string(newghost);
   
-  msg = "```md\nAttack\n=======\nYou dealt " + dmg + " Damage\nThe Ghost now has " + (ghost.Health -1) + " Health```";
+  msg = "```md\nAttack\n=======\nYou dealt " + dmg + " Damage\nThe Ghost now has " + (ghost.Health - 2) + " Health```";
 }
 
 function checkHealth() {
